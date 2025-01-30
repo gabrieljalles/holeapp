d052953b4b06be37bb0ed4e4bced4522c406a7a6
gabrieljalles <gabriel.jalleswork@hotmail.com>
2025-01-30 15:25:54 -0300

    build : error corretion nextConfig

--- next-hole/next.config.mjs
+++ next-hole/next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL || "https",
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || "backend-holeapp-nest.onrender.com",
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME,
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT,
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || "backend-holeapp-nest.onrender.com",
         pathname: process.env.NEXT_PUBLIC_IMAGE_PATHNAME || "/spothole/uploads/**",
       },
     ],
