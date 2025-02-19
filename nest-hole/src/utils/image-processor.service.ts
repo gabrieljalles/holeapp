0691eab224920d6acb59f3fa0f7af3e4eb27fa55
gabrieljalles <gabriel.jalleswork@hotmail.com>
2025-02-19 20:04:59 -0300

    feat correction: version imagemin

--- nest-hole/src/utils/image-processor.service.ts
+++ nest-hole/src/utils/image-processor.service.ts
@@ -1,5 +1,5 @@
 import { extname } from 'path';
-import * as pngToJpeg from 'png-to-jpeg';
+import pngToJpeg from 'png-to-jpeg';
 
 export async function processImage(
     file: Express.Multer.File
@@ -7,7 +7,7 @@ export async function processImage(
     const imagemin = (await import('imagemin')).default;
     const imageminMozjpeg = (await import('imagemin-mozjpeg')).default;
 
-    
+
     const extension = extname(file.originalname).toLowerCase();
     const quality = 50;
 
